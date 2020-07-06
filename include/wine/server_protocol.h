47def799869bb21064fde3611c0df34d20ef0a3a
Zebediah Figura <zfigura@codeweavers.com>
2020-07-06 16:11:23 -0500

    server, ntdll: Implement message waits.

--- include/wine/server_protocol.h
+++ include/wine/server_protocol.h
@@ -5452,6 +5452,17 @@ struct get_esync_fd_reply
 };
 
 
+struct esync_msgwait_request
+{
+    struct request_header __header;
+    int          in_msgwait;
+};
+struct esync_msgwait_reply
+{
+    struct reply_header __header;
+};
+
+
 enum request
 {
     REQ_new_process,
@@ -5730,6 +5741,7 @@ enum request
     REQ_get_next_thread,
     REQ_create_esync,
     REQ_get_esync_fd,
+    REQ_esync_msgwait,
     REQ_NB_REQUESTS
 };
 
@@ -6013,6 +6025,7 @@ union generic_request
     struct get_next_thread_request get_next_thread_request;
     struct create_esync_request create_esync_request;
     struct get_esync_fd_request get_esync_fd_request;
+    struct esync_msgwait_request esync_msgwait_request;
 };
 union generic_reply
 {
@@ -6294,11 +6307,12 @@ union generic_reply
     struct get_next_thread_reply get_next_thread_reply;
     struct create_esync_reply create_esync_reply;
     struct get_esync_fd_reply get_esync_fd_reply;
+    struct esync_msgwait_reply esync_msgwait_reply;
 };
 
 /* ### protocol_version begin ### */
 
-#define SERVER_PROTOCOL_VERSION 743
+#define SERVER_PROTOCOL_VERSION 744
 
 /* ### protocol_version end ### */
 
